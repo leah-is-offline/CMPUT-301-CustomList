e6184cf39058c17b55d2f8025c8c66730015a247
Leah Copeland <59579748+leah-is-offline@users.noreply.github.com>
2020-02-28 09:01:48 -0800

    updated Junit Testing

--- app/src/main/java/com/example/simpleparadox/listycity/City.java
+++ app/src/main/java/com/example/simpleparadox/listycity/City.java
@@ -1,19 +1,21 @@
 package com.example.simpleparadox.listycity;
 
-public class City {
+public class City implements Comparable<City> {
+
     private String city;
     private String province;
-
     City(String city, String province){
         this.city = city;
         this.province = province;
     }
-
     String getCityName(){
         return this.city;
     }
-
     String getProvinceName(){
         return this.province;
+    }
+    @Override
+    public int compareTo(City city) {
+        return this.city.compareTo(city.getCityName());
     }
 }
